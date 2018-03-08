74e29ca01195b1224bbf5948d138e5fae9310be4
BJeffreyDarmawan <bernardusjeffrey747@gmail.com>
2018-03-08 20:34:15 +0700

    update validation

--- week 1/Pramana_Hotel/validate.js
+++ week 1/Pramana_Hotel/validate.js
@@ -6,22 +6,39 @@ function validateForm() {
         return false;
     }
     
-    x = document.forms["myform"]["email"].value;
+    x = document.forms["myform"]["email"].value;alert(x);
     if (x == "") {
         alert("Email must be filled out");
+        return false;
+    }else if (validateEmail(x) == false){  //sir why cant just validateEmail(x)..
+        alert("Invalid email address");
         return false;
     }
     
     x = document.forms["myform"]["phone"].value - 0;
     if (x.typeof == "string") {
-        alert("Phone must be filled out");
+        alert("You inserted wrong phone number");
+        return false;
+    } else if ( == "string") {
+        alert("You inserted wrong phone number");
         return false;
     }
     
     var d = new Date();
-    var y = d.getFullYear() + "-" + d.getMonth()+1 + "-" + d.getDate();
+    var y = d.getFullYear() + "-";
+    if(d.getMonth()+1 < 10)
+        y = y + "0" + (d.getMonth()+1);
+    else 
+        y = y + (d.getMonth()+1);
+    
+    if(d.getDate() < 10)
+        y = y + "-0" + d.getDate();
+    else 
+        y = y + "-" + d.getDate();
+    
     x = document.forms["myform"]["date"].value;
     alert(y);
+    alert(x);
     if(x < y) {
         alert("Date must be filled out");
         return false;
@@ -38,4 +55,9 @@ function validateForm() {
         alert("Please choose room type");
         return false;
     }
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
